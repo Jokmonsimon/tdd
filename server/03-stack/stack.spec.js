0d74cd78897825a69c0e1051f73b57fa75445288
Afande Ojok <simonpeter.oj@gmail.com>
2022-11-09 17:36:54 +0300

    stack size is zero when pushed and popped

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,25 +1,25 @@
 const stackFactory = () => {
-  let empty = true;
   let count = 0;
 
   return {
-    isEmpty: () => empty,
+    isEmpty: () => count === 0,
     size: () => count,
-    push: () => { 
-      empty = false; 
+    push: () => {  
       count += 1;
     },
-    pop: () => { empty = true}
+    pop: () => { 
+      count -= 1;
+    }
   }
 };
+
+let stack;
 
 describe('the stack canary spec', () => {
   it('shows the infrastructure works', () => {
     expect(true).toBe(true);
   });
 });
-
-let stack;
 
 describe('a stack', () => {
   beforeEach(() => {
@@ -44,8 +44,11 @@ describe('a stack', () => {
     stack.pop();
     expect(stack.isEmpty()).toBe(true);
   });
-  it.todo('stack is empty when pushed and popped');
-  it.todo('stack size is 0 when pushed and popped');
+  it('stack size is 0 when pushed and popped', () => {
+    stack.push();
+    stack.pop();
+    expect(stack.size()).toBe(0);
+  });
   it.todo('throws overflow error when pushing to a stack at full capacity');
   it.todo('throw underflow error when popping an empty stack');
   it.todo('pops the same one item when pushed');
